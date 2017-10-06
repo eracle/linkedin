94ba47614473cba414983e107e11ee64226a1fc8
eracle <eracle@posteo.eu>
2017-10-06 13:08:42 +0200

    added my version of chrome headless
    Former-commit-id: dc313bb610f3e031d5f1d101d5aa67148bd2e62b

--- linkedin/middlewares.py
+++ linkedin/middlewares.py
@@ -1,27 +1,29 @@
 from scrapy.http import HtmlResponse
-from selenium_utils import *
-import linkedin
 from scrapy.utils.python import to_bytes
+from selenium.webdriver.common.keys import Keys
+
+from selenium_utils import get_by_xpath
+
 
 class Selenium(object):
     def process_request(self, request, spider):
         driver = spider.driver
 
-        logger.info('SeleniumMiddleware - getting the page')
+        print('SeleniumMiddleware - getting the page')
         driver.get(request.url)
 
-        logger.info('SeleniumMiddleware - click more options')
+        print('SeleniumMiddleware - click more options')
         more_option = get_by_xpath(driver, '//div/div/button[@class="more-options dropdown-caret"]')
         more_option.send_keys(Keys.NULL)
         more_option.click()
 
-        logger.info('SeleniumMiddleware - wait for names')
+        print('SeleniumMiddleware - wait for names')
         name = get_by_xpath(driver, '//ul[@class="browse-map-list"]/li/h4/a')
         name.send_keys(Keys.NULL)
 
         #request.meta['driver'] = self.driver  # to access driver from response
 
-        logging.info('SeleniumMiddleware - retrieving body')
+        print('SeleniumMiddleware - retrieving body')
         body = to_bytes(driver.page_source)  # body must be of type bytes
         return HtmlResponse(driver.current_url, body=body, encoding='utf-8', request=request)
 
